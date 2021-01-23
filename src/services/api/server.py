7b0ba5fca377a16c94fee44e290502b4c9630757
javigilantu <javigilantu@gmailcom>
2021-01-23 18:33:15 +0100

    Clase, no consigo que me devuelva la grafica

--- src/services/api/server.py
+++ src/services/api/server.py
@@ -12,6 +12,7 @@ def read_json(fullpath):
         json_readed = json.load(json_file_readed)
     return json_readed
 
+
 # Mandatory
 app = Flask(__name__)  # __name__ --> __main__  
 
@@ -34,6 +35,7 @@ def give_s():
     t_d_averages = read_json(fullpath=settings_file)
     S = request.args['S']
     if S == "d102159467":
+        downloader()
         return t_d_averages 
     else:
         return "I am affarid not, try again"
