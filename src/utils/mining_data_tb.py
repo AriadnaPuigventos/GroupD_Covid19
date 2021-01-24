497f99ce6105d614e2bd0718e247599b77838f70
AriadnaPuigventos <ariadnapuigventos@gmail.com>
2021-01-24 20:54:15 +0100

    update ariadna and ariadna-b1

--- src/utils/mining_data_tb.py
+++ src/utils/mining_data_tb.py
@@ -3,8 +3,12 @@ Date: 18/01/2021
 Functionally about collect, clean and wrangling methods data.
 '''
 import pandas as pd
+import missingno as msno 
 
 def countries(df):
     mask = df["location"] == "Venezuela" and "Spain"
     return mask
-  
+
+#Vizualitazion Correlation Matrix NaN's
+def visual_NaNs():
+    #msno.matrix()
